08b39fe362c9cb976ab84f75e4126c46d638625c
kolesnikovaolga <kolesnikovaolga@debian.debian.knu>
2017-10-29 12:12:48 +0200

    lab.1.7.1

---
+++ printing.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+float val1, val2, val3, val4, val5;
+cin >> val1 ;
+cin >> val2 ;
+
+cout << val1 << endl;
+cout << val1 << "0"<<endl;
+cout << val2 << endl;
+cout << val2  << endl;
+cout << (int)val2 <<endl;
+return 0;
+}
