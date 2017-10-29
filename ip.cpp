1d9ca089d395494e91924767d8473e19985f686c
kolesnikovaolga <kolesnikovaolga@debian.debian.knu>
2017-10-29 13:19:30 +0200

    lab.1.7.3

---
+++ ip.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+int numb1, numb2, numb3, numb4;
+
+cin >> numb1;
+cin >> numb2;
+cin >> numb3;
+cin >> numb4;
+
+if (0<= numb1,numb2,numb3, numb4<=255){
+cout << numb1 <<'.'<<numb2<< '.' << numb3 << '.' <<numb4 << endl;}
+return 0;
+}
